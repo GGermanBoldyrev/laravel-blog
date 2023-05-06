ba63e14532e1158c1cbe2fc4193a915b585b3fb6
GGermanBoldyrev <boldyrev_german@mail.ru>
2023-05-06 03:44:39 +0300

    end of a day

--- resources/views/post/edit.blade.php
+++ resources/views/post/edit.blade.php
@@ -7,31 +7,72 @@
             <!--Кнопка назад-->
             <x-back_posts_button/>
             <!--Блок с формой-->
-            <div class="flex items-center justify-center min-h-screen-minus-header">
+            <div class="flex flex-col items-center justify-center min-h-screen-minus-header">
+                <!--Заголовок-->
+                <div class="text-5xl text-center">Edit your post</div>
                 <!--Форма создания поста-->
                 <form action="{{ route('posts.store') }}" method="POST" class="w-[600px]">
                     @csrf
-                    <!--Заголовок-->
-                    <div class="text-5xl text-center">Edit your post</div>
+
                     <!--Ввод title-->
-                    <div class="flex flex-col mt-[30px]">
-                        <x-form-label for="title">Enter new title</x-form-label>
-                        <x-form-textarea id="title" placeholder="New title" rows="2">{{ $post->title }}</x-form-textarea>
+                    <div class="flex flex-col mt-[50px]">
+                        <!--Лейбл для title-->
+                        <label for="update-post-title" class="text-xl mb-[5px]">Change the title *</label>
+                        <!--Ввод текста title-->
+                        <textarea id="update-post-title" name="title" type="text" rows="2"
+                                  {{--Если есть ошибки валидации--}}
+                                  @if($errors->getBag('default')->first('title'))
+                                      placeholder="{{ $errors->getBag('default')->first('title') }}"
+                                  class="bg-background border rounded border-black outline-0 placeholder-red-800
+                                             p-1.5 resize-none"
+                                  @else
+                                      placeholder="Title"
+                                  class="bg-background border rounded border-black outline-0 placeholder-black p-1.5
+                                             resize-none"
+                                  @endif
+                        >{{ $post->title }}</textarea>
                     </div>
+
                     <!--Ввод content-->
                     <div class="flex flex-col mt-[30px]">
-                        <x-form-label for="content">Enter new text</x-form-label>
-                        <x-form-textarea id="content" placeholder="New text" rows="8">{{ $post->content }}</x-form-textarea>
+                        <!--Лейбл для content-->
+                        <label for="update-post-content" class="text-xl mb-[5px]">Change the content *</label>
+                        <!--Ввод текста для content-->
+                        <textarea id="update-post-content" name="content" type="text" rows="8"
+                                  {{--Если есть ошибки валидации--}}
+                                  @if($errors->getBag('default')->first('content'))
+                                      placeholder="{{ $errors->getBag('default')->first('content') }}"
+                                  class="bg-background border rounded border-black outline-0 placeholder-red-800
+                                             p-1.5 resize-none"
+                                  @else
+                                      placeholder="Content"
+                                  class="bg-background border rounded border-black outline-0 placeholder-black p-1.5
+                                             resize-none"
+                                  @endif
+                        >{{ $post->content }}</textarea>
                     </div>
+
                     {{--Прикрепить картинку--}}
-                    <div class="mt-5">Add an image</div>
+                    <div class="flex flex-col mt-[30px]">
+                        <!--Лейбл для картинки-->
+                        <label for="update-post-image" class="text-xl mb-[5px]">Change an image *</label>
+                        <!--Ввод картинки-->
+                        <input name="img" id="update-post-image" type="text" class="bg-background border rounded
+                                border-black outline-0 placeholder-black p-1.5 w-1/2" placeholder="Image"
+                            @if($post->img != 'no-image')
+                                {{ $post->img }}
+                            @endif
+                        >
+                    </div>
+
                     {{--Чекбокс вернуться к посту--}}
-                    <div>
-                        <input type="checkbox" name="checkbox" id="checkbox" class="mt-5">
+                    <div class="mt-[30px]">
+                        <input type="checkbox" name="checkbox" id="checkbox">
                         <label for="checkbox" class="ml-1">Go to your post</label>
                     </div>
+
                     {{--Контейнер для кнопки submit--}}
-                    <div class="w-full flex justify-center mt-5">
+                    <div class="w-full flex justify-center mt-[30px]">
                         <!--Кнопка submit-->
                         <x-form-submit/>
                     </div>
